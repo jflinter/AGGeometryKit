b57868479582f6cc9324dfbe155c3c024a828ddf
jflinter <jflinter11@gmail.com>
2016-04-14 03:02:51 -0400

    more mirror tweaks

--- AGGeometryKit/AGKQuad.m
+++ AGGeometryKit/AGKQuad.m
@@ -159,32 +159,44 @@ AGKQuad AGKQuadInsetBottom(AGKQuad q, CGFloat inset)
 
 AGKQuad AGKQuadMirror(AGKQuad q, BOOL x, BOOL y)
 {
-    AGKQuad mirroredQ = q;
-    CGPoint center = AGKQuadGetCenter(q);
     if(x)
     {
-        mirroredQ.tl.x = (2 * center.x) - q.tr.x;
-        mirroredQ.tl.y = q.tr.y;
-        mirroredQ.tr.x = (2 * center.x) - q.tl.x;
-        mirroredQ.tr.y = q.tl.y;
-        mirroredQ.bl.x = (2 * center.x) - q.br.x;
-        mirroredQ.bl.y = q.br.y;
-        mirroredQ.br.x = (2 * center.x) - q.bl.x;
-        mirroredQ.br.y = q.bl.y;
-        q = mirroredQ;
+        q = AGKQuadMirrorHorizontal(q);
     }
     if(y)
     {
-        mirroredQ.tl.y = (2 * center.y) - q.tr.y;
-        mirroredQ.tl.x = q.tr.x;
-        mirroredQ.tr.y = (2 * center.y) - q.tl.y;
-        mirroredQ.tr.x = q.tl.x;
-        mirroredQ.bl.y = (2 * center.y) - q.br.y;
-        mirroredQ.bl.x = q.br.x;
-        mirroredQ.br.y = (2 * center.y) - q.bl.y;
-        mirroredQ.br.x = q.bl.x;
-        q = mirroredQ;
+        q = AGKQuadMirrorVertical(q);
     }
+    return q;
+}
+
+AGKQuad AGKQuadMirrorHorizontal(AGKQuad q)
+{
+    AGKQuad mirroredQ = q;
+    CGPoint center = AGKQuadGetCenter(q);
+    mirroredQ.tl.x = (2 * center.x) - q.tr.x;
+    mirroredQ.tl.y = q.tr.y;
+    mirroredQ.tr.x = (2 * center.x) - q.tl.x;
+    mirroredQ.tr.y = q.tl.y;
+    mirroredQ.bl.x = (2 * center.x) - q.br.x;
+    mirroredQ.bl.y = q.br.y;
+    mirroredQ.br.x = (2 * center.x) - q.bl.x;
+    mirroredQ.br.y = q.bl.y;
+    return mirroredQ;
+}
+
+AGKQuad AGKQuadMirrorVertical(AGKQuad q)
+{
+    AGKQuad mirroredQ = q;
+    CGPoint center = AGKQuadGetCenter(q);
+    mirroredQ.tl.y = (2 * center.y) - q.tr.y;
+    mirroredQ.tl.x = q.tr.x;
+    mirroredQ.tr.y = (2 * center.y) - q.tl.y;
+    mirroredQ.tr.x = q.tl.x;
+    mirroredQ.bl.y = (2 * center.y) - q.br.y;
+    mirroredQ.bl.x = q.br.x;
+    mirroredQ.br.y = (2 * center.y) - q.bl.y;
+    mirroredQ.br.x = q.bl.x;
     return mirroredQ;
 }
 
